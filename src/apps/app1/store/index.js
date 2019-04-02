4292ffe611a43b7c7004070d23a3462f67d827bb
xiedrsz <1805713222@qq.com>
2019-04-02 17:01:53 +0800

    state代理
    引入state代理，防止页面刷新后跳出时状态被清除

--- src/apps/app1/store/index.js
+++ src/apps/app1/store/index.js
@@ -1,7 +1,6 @@
 /**
  * view或者业务逻辑发出，dispatch派发事件给actions=>actions 会commit事件给mutations,操作state
  */
-
 import Vue from 'vue'
 import Vuex from 'vuex'
 import actions from './actions'
@@ -11,9 +10,20 @@ import mutations from './mutations'
 
 Vue.use(Vuex)
 
+// 引入代理防止页面跳出后状态被清楚
+let mstate = new Proxy(state, {
+  set (target, key, value, receiver) {
+    let oldState = sessionStorage.getItem('mstate') || '{}'
+    oldState = JSON.parse(oldState)
+    oldState[key] = value
+    sessionStorage.setItem('mstate', JSON.stringify(oldState))
+    return Reflect.set(target, key, value, receiver)
+  }
+})
+
 export default new Vuex.Store({
   actions,
   getters,
-  state,
+  state: mstate,
   mutations
 })
--- src/apps/app1/store/state.js
+++ src/apps/app1/store/state.js
@@ -2,7 +2,11 @@
  * state 相当于变量的储存
  * export default state 把state暴露出去
  */
-export default {
+// 优先从缓存中读取
+let oldState = sessionStorage.getItem('mstate')
+let state = oldState ? JSON.parse(oldState) : {
   // Todo
   page: ''
 }
+
+export default state
